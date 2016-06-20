7a77a31fcd9def9a34fd727d49398639101904d4
Sebastian Nyholm <sebastian@vaadin.com>
2016-06-20 17:14:52 +0300

    Renames project related files and classes to ChartsTheming. Adds the client compiler to the pom.xml

--- src/main/java/com/vaadin/sebastian/ChartsThemingUI.java
+++ src/main/java/com/vaadin/sebastian/ChartsThemingUI.java
@@ -14,38 +14,40 @@ import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
 /**
- * This UI is the application entry point. A UI may either represent a browser window 
- * (or tab) or some part of a html page where a Vaadin application is embedded.
+ * This UI is the application entry point. A UI may either represent a browser
+ * window (or tab) or some part of a html page where a Vaadin application is
+ * embedded.
  * <p>
- * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
- * overridden to add component to the user interface and initialize non-component functionality.
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is
+ * intended to be overridden to add component to the user interface and
+ * initialize non-component functionality.
  */
-@Theme("mytheme")
-@Widgetset("com.vaadin.sebastian.MyAppWidgetset")
-public class MyUI extends UI {
+@Theme("chartstheming")
+@Widgetset("com.vaadin.sebastian.ChartsThemingWidgetset")
+public class ChartsThemingUI extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         final VerticalLayout layout = new VerticalLayout();
-        
+
         final TextField name = new TextField();
         name.setCaption("Type your name here:");
 
         Button button = new Button("Click Me");
-        button.addClickListener( e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
-                    + ", it works!"));
+        button.addClickListener(e -> {
+            layout.addComponent(
+                    new Label("Thanks " + name.getValue() + ", it works!"));
         });
-        
+
         layout.addComponents(name, button);
         layout.setMargin(true);
         layout.setSpacing(true);
-        
+
         setContent(layout);
     }
 
-    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
-    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
-    public static class MyUIServlet extends VaadinServlet {
+    @WebServlet(urlPatterns = "/*", name = "ChartsThemingServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = ChartsThemingUI.class, productionMode = false)
+    public static class ChartsThemingServlet extends VaadinServlet {
     }
 }
